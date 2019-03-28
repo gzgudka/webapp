cdf643fa7c5d8da9ec99f85995a438f30a4d7c50
Greg Zgudka <greg.zgudka@loricahealth.com>
2019-03-28 17:21:14 +1100

    add docker-compose

--- component-tests/src/test/java/example/ComponentTest.java
+++ component-tests/src/test/java/example/ComponentTest.java
@@ -1,17 +1,27 @@
 package example;
 
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
 
 import static io.restassured.RestAssured.when;
 import static org.hamcrest.core.Is.is;
 
 public class ComponentTest {
 
+    @BeforeAll
+    public static void setUp() {
+        RestAssured.baseURI = Optional.ofNullable(System.getenv("BASE_URL")).orElse("http://localhost:8080");
+    }
+
     @Test
     public void shouldSayHelloWorld() {
         when()
-            .get("http://localhost:8080/hello")
+            .get("/hello")
         .then()
+            .log().ifError()
             .statusCode(200)
             .body(is("Hello World"));
     }
