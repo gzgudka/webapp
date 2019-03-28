36c0a1f675ccfb48263455c67af828f8b41c94f0
Greg Zgudka <greg.zgudka@loricahealth.com>
2019-03-28 21:36:01 +1100

    clean up poms

--- app/src/test/java/example/HelloServiceTest.java
+++ app/src/test/java/example/HelloServiceTest.java
@@ -1,9 +1,9 @@
 package example;
 
-import org.hamcrest.core.Is;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 
 public class HelloServiceTest {
 
@@ -16,7 +16,7 @@ public class HelloServiceTest {
         String msg = new HelloService().sayHello(name);
 
         // then
-        assertThat(msg, Is.is("Hello Greg"));
+        assertThat(msg, is("Hello Greg"));
     }
 
 }
